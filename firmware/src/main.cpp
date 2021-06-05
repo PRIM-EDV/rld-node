93c6248763f3efce5f5fa0ed5bb0de9da7cb2b40
Lucas Mösch <lucasmoesch@live.de>
2021-06-05 04:55:16 +0200

    WIP

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,11 +1,10 @@
 #include "../board/board.hpp"
-#include "../lib/ai-thinker/sx127x.hpp"
+// #include "../lib/ai-thinker/sx127x.hpp"
 
 using namespace Board;
 
 int main()
 {
-    
     while (true)
     {
 
